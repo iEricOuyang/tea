da5c28db29accd6cc3d8b7fd2f853796b593dcc5
欧阳德才 <584278919@qq.com>
2017-01-03 15:44:53 +0800

    utils.js修改

--- src/utils.js
+++ src/utils.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
 var util={
-    host:'http://211.149.162.113:8880/',//http://211.149.162.113:8880/
+    host:'http://211.149.162.113:8880/teaculture/',//http://192.168.1.175:8098/teaculture/  http://211.149.162.113:8880/
     imgHost:'http://file.static.kantakj.com:10000/',
     ajax:function(host,url,params,callback){
+
         if(host==''){
             Vue.http.post(host+url,params).then(function(response){
                     callback(response);
