fa42d6dbef8ce4c5ab63c3172b80d33b80ee954a
欧阳德才 <584278919@qq.com>
2017-12-21 00:24:16 +0800

    modify

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -54,7 +54,7 @@ module.exports = {
         query: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]'),
-          publicPath: '/'
+          publicPath: '../'
         }
       },
       {
